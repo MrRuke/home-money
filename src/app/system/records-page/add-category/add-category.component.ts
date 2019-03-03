7e7a6a5eaa6c462d766bffdca93df7ebef5376c6
Eduard Rudenko (MrRuke) <ruyteks@gmail.com>
2019-03-03 21:56:04 +0300

    Оповещение об успешном добавлении

--- src/app/system/records-page/add-category/add-category.component.ts
+++ src/app/system/records-page/add-category/add-category.component.ts
@@ -1,21 +1,27 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {NgForm} from "@angular/forms";
 import {CategoriesService} from "../../shared/services/categories.service";
 import {Category} from "../../shared/models/category.model";
+import {Message} from "../../../shared/models/message.model";
 
 @Component({
   selector: 'app-add-category',
   templateUrl: './add-category.component.html',
   styleUrls: ['./add-category.component.scss']
 })
-export class AddCategoryComponent {
+export class AddCategoryComponent implements OnInit {
 
   @Output() onCategoryAdd = new EventEmitter<Category>();
 
+  message: Message;
+
   constructor(private categoriesService: CategoriesService) { }
 
+  ngOnInit() {
+    this.message = new Message('', 'alert-success');
+  }
+
   onSubmit(form: NgForm) {
-    console.log(form);
     let { name, capacity } = form.value;
     if (capacity < 0) capacity *= -1;
 
@@ -25,6 +31,8 @@ export class AddCategoryComponent {
       .subscribe((category: Category) => {
         form.reset();
         form.form.patchValue({capacity: 1});
+        this.message.text = 'Категория добавлена.';
+        window.setTimeout(()=> this.message.text = '', 3000);
         this.onCategoryAdd.emit(category);
       })
   }
