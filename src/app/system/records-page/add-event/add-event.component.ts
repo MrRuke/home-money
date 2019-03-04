b88d2d46d94d0f92f01974cd743a2a7b3674897f
Eduard Rudenko (MrRuke) <ruyteks@gmail.com>
2019-03-04 22:33:37 +0300

    Добавление событий на страницу записи

--- src/app/system/records-page/add-event/add-event.component.ts
+++ src/app/system/records-page/add-event/add-event.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Category} from "../../shared/models/category.model";
+import {NgForm} from "@angular/forms";
+import * as moment from 'moment';
+import {AppEvent} from "../../shared/models/event.model";
 
 @Component({
   selector: 'app-add-event',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEventComponent implements OnInit {
 
+  @Input() categories: Category[] = [];
+
+  types = [
+    {type: 'income', label : 'Доход'},
+    {type: 'outcome', label : 'Расход'}
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSubmit(form: NgForm) {
+    let {amount, description, category, type} = form.value;
+    if (amount< 0) amount *= -1;
+
+    const event = new AppEvent(
+      type, amount, +category, moment().format('DD.MM.YYYY HH:mm:ss'), description
+    );
+  }
 }
