9c6b90372fc7d26bac87c570a0b67b0fc0081787
Eduard Rudenko (MrRuke) <ruyteks@gmail.com>
2019-03-06 23:39:04 +0300

    Вывод истории списком

--- src/app/system/history-page/history-events/history-events.component.ts
+++ src/app/system/history-page/history-events/history-events.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Category} from "../../shared/models/category.model";
+import {AppEvent} from "../../shared/models/event.model";
 
 @Component({
   selector: 'app-history-events',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HistoryEventsComponent implements OnInit {
 
+  @Input() categories: Category = [];
+  @Input() events: AppEvent = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.events.forEach((e) => {
+      e.catName = this.categories.find(c => c.id === e.category).name;
+    });
+  }
+
+  getEventClass(e: AppEvent) {
+    return {
+      'label' : true,
+      'label-danger': e.type === 'outcome',
+      'label-success ': e.type === 'income',
+    }
   }
 
 }
