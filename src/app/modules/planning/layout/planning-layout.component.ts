931444a86dce9d6ced2bdceade9306db1bd3202c
Eduard Rudenko <ruyteks@gmail.com>
2021-02-23 14:05:28 +0300

    planning page updated

--- src/app/modules/planning/layout/planning-layout.component.ts
+++ src/app/modules/planning/layout/planning-layout.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { BemModifiers } from '@app/old/shared/core/bem';
 import { PlanningUseCases } from '../planning.usecases';
-import { PlanningViewModel } from '../planning.viewmodel';
+import { PlanningView, PlanningViewModel } from '../planning.viewmodel';
 
 @Component({
   selector: 'app-planning-layout',
   templateUrl: './planning-layout.component.html',
-  styleUrls: ['./planning-layout.component.scss']
+  styleUrls: ['./planning-layout.component.scss'],
 })
 export class PlanningLayoutComponent implements OnInit {
   public readonly planning = this.viewModel.selectPlanning();
@@ -13,9 +14,22 @@ export class PlanningLayoutComponent implements OnInit {
   constructor(
     private viewModel: PlanningViewModel,
     private useCases: PlanningUseCases,
-  ) { }
+  ) {
+  }
 
   public ngOnInit(): void {
     this.useCases.loadValues().subscribe();
+  }
+
+  public getProgressBarModifiers(percent: number): BemModifiers {
+    return {
+      low: percent < 60,
+      medium: percent > 60 && percent < 100,
+      high: percent >= 100,
+    };
+  }
+
+  public trackByPlanning(index: number): number {
+    return index;
   }
 }
