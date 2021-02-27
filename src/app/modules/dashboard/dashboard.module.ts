93bfbccd5efaaa44de04d3c6a9d757d6df321c31
Eduard Rudenko <ruyteks@gmail.com>
2021-02-27 23:06:25 +0300

    dashboard card

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { BemModule } from 'angular-bem';
 import { DashboardLayoutComponent } from './layout/dashboard-layout.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
@@ -10,6 +12,8 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
     CommonModule,
     DashboardRoutingModule,
     BemModule,
+    MatButtonModule,
+    MatIconModule,
   ],
 })
 export class DashboardModule {
