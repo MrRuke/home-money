9f40977ef096498f424c73ce4a4618c7d688fef2
Eduard Rudenko <ruyteks@gmail.com>
2024-03-04 00:15:12 +0200

    Angular material 14 to 15

--- src/app/modules/planning/planning.module.ts
+++ src/app/modules/planning/planning.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
+import { MatLegacyCardModule as MatCardModule } from '@angular/material/legacy-card';
 import { CostPipeModule } from '@app/services/cost.pipe.module';
 import { BemModule } from 'angular-bem';
 import { PlanningLayoutComponent } from './layout/planning-layout.component';
