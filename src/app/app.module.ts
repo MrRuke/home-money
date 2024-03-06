dc97726d2c35fffadeef519f295765aa65c37506
Eduard Rudenko <ruyteks@gmail.com>
2024-03-07 01:00:00 +0200

    ngrx installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [
@@ -22,6 +23,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     TranslateModule.forRoot(),
     HeaderModule,
     MainMenuModule,
+    StoreModule.forRoot({}, {}),
   ],
   providers: [
     CommonModule,
